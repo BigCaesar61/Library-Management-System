723ddb337f21a080bbe2eef7406392d5a1b82f7b
Martin Leone <martinnikolaslll@gmail.com>
2024-10-16 11:24:28 -0400

    Create Section class to manage books and availability

--- Library_Management_System.js
+++ Library_Management_System.js
@@ -1,6 +1,6 @@
 //Task 1: Create a Book Class
 
-class book {
+class Book {
     constructor(title, author, ISBN ) {
         this.title = title;
         this.author = author;
@@ -13,11 +13,36 @@ class book {
     }
 
     get _isAvailable() {
-        return this._isAvailable;
+        return this._isAvailable; //tells us if the book is available
     }
 
     set _isAvailable(status) {
-        this._isAvailable = status;
+        this._isAvailable = status; //alows you to set status of the book (true or false)
     }
 
+}
+
+
+//Task 2: Create a Section Class
+
+class Section {
+    constructor(name,books) {
+        this.name = name;
+        this.books = [];
+
+    }
+
+    addBook(book) {
+        this.book.push(book); //method that pushes new books into our book array
+    }
+
+    getAvailableBooks() {
+        return this.books.filter(book => book._isAvailable).length; //number of books available
+    }
+
+    listBooks() {
+        this.books.forEach(book => {
+            console.log(`Title: ${book.title}, Available: ${book.isAvailable ? 'Yes' : 'No'}`)
+        });
+    }
 }
